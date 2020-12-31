086a12c4b3c73407f8673afbf0b5760ef676c8ec
Kevin Rozmiarek <kevrozmiarek@gmail.com>
2020-12-30 17:39:53 -0700

    Finished Hardware/Basic Display/Sample Insall

--- k_type_reader/k_type_reader.ino
+++ k_type_reader/k_type_reader.ino
@@ -1,30 +1,57 @@
 #include <Wire.h>
 #include <Adafruit_ADS1015.h>
+
+#define AREF 4.096           // set to AREF, typically board voltage like 3.3 or 5.0
+#define ADC_RESOLUTION 16  // set to ADC bit resolution, 10 is default
  
-Adafruit_ADS1015 ads1015;
- 
+Adafruit_ADS1115 ads1115;
+
+float reading1, reading2, reading3, reading4, voltage1, voltage2, voltage3, voltage4, temperature1, temperature2, temperature3, temperature4;
+
+float get_voltage(int raw_adc) {
+  return raw_adc * 0.125/1000; 
+  //return raw_adc * (AREF / (pow(2, ADC_RESOLUTION)-1));  
+}
+
+float get_temperature(float voltage) {
+  return (voltage - 1.25) / 0.005;
+}
+
 void setup(void)
 {
   Serial.begin(9600);
   Serial.println("Hello!");
   
   Serial.println("Getting single-ended readings from AIN0..3");
-  Serial.println("ADC Range: +/- 6.144V (1 bit = 3mV)");
-  ads1015.begin();
+  ads1115.setGain(GAIN_ONE);     // 1x gain   +/- 4.096V  1 bit = 0.125mV
+  //Serial.println("ADC Range: +/- 6.144V (1 bit = 3mV)");
+  ads1115.begin();
 }
  
 void loop(void)
 {
-  int16_t adc0, adc1, adc2, adc3;
+  //int16_t adc0, adc1, adc2, adc3;
  
-  adc0 = ads1015.readADC_SingleEnded(0);
-  adc1 = ads1015.readADC_SingleEnded(1);
-  adc2 = ads1015.readADC_SingleEnded(2);
-  adc3 = ads1015.readADC_SingleEnded(3);
-  Serial.print("AIN0: "); Serial.println(adc0);
-  Serial.print("AIN1: "); Serial.println(adc1);
-  Serial.print("AIN2: "); Serial.println(adc2);
-  Serial.print("AIN3: "); Serial.println(adc3);
+  reading1 = ads1115.readADC_SingleEnded(0);
+  reading2 = ads1115.readADC_SingleEnded(1);
+  reading3 = ads1115.readADC_SingleEnded(2);
+  reading4 = ads1115.readADC_SingleEnded(3);
+
+  voltage1 = get_voltage(reading1);
+  voltage2 = get_voltage(reading2);
+  voltage3 = get_voltage(reading3);
+  voltage4 = get_voltage(reading4);
+
+  temperature1 = get_temperature(voltage1);
+  temperature2 = get_temperature(voltage2);
+  temperature3 = get_temperature(voltage3);
+  temperature4 = get_temperature(voltage4);
+  
+  //Serial.print("ktype1: "); Serial.println(temperature1);
+  //Serial.print("ktype2: "); Serial.println(temperature2);
+  //Serial.print("ktype3: "); Serial.println(temperature3);
+  //Serial.print("ktype4: "); Serial.println(temperature4);
+  Serial.print((String)temperature1 + "," + (String)temperature2 + "," + (String)temperature3 + "," + (String)temperature4 + ",");
   Serial.println(" ");
   
   delay(1000);
--- recorder_gui.py
+++ recorder_gui.py
@@ -9,10 +9,25 @@ import serial as serial
 import tkinter as tk
 import datetime 
 
+try:
+    serialPort = 'COM6' #Will change based on port plugged into
+    baudRate = 9600
+    ser = serial.Serial(serialPort , baudRate, timeout=0, writeTimeout=0) #ensure non-blocking
+    
+except serial.SerialException:
+    print('Port is open')    
 
-serialPort = "/dev/ttyACM0" #Will change based on port plugged into
-baudRate = 9600
-ser = serial.Serial(serialPort , baudRate, timeout=0, writeTimeout=0) #ensure non-blocking
+def disconnect():
+    ser.close()
+    root.destroy()
+
+def restartSerial():
+    global ser
+    log.insert(tk.END, 'Restarting comms')
+    ser.close()
+    serialPort = 'COM6' #Will change based on port plugged into
+    baudRate = 9600
+    ser = serial.Serial(serialPort, baudRate, timeout=0, writeTimeout=0) #ensure non-blocking
 
 #make a TkInter Window
 root = tk.Tk()
@@ -20,47 +35,116 @@ root.wm_title("Temperatue Recorder")
 
 # make a scrollbar
 scrollbar = tk.Scrollbar(root)
-scrollbar.pack()#side = RIGHT, fill = Y)
+scrollbar.pack(side = tk.RIGHT, fill = tk.Y)
 
 # make a text box to put the serial output
-log = tk.Text ( root, width=30, height=30, takefocus=0)
+log = tk.Text(root, width=60, height=30, takefocus=0)
 log.pack()
 
 # attach text box to scrollbar
 log.config(yscrollcommand=scrollbar.set)
 scrollbar.config(command=log.yview)
 
-#make a buffer
+#Button to restart serial
+restartButton = tk.Button(root, text="Restart Serial Comms", command = restartSerial)
+restartButton.pack()
+
+#Button to close correctly
+closeButton = tk.Button(root, text="Exit", command = disconnect)
+closeButton.pack()
+
+#Temperature Displays
+workingTemp1 = ""
+workingTemp2 = ""
+workingTemp3 = ""
+workingTemp4 = ""
+
+workingTemp1Display = tk.Label(root,text="Temp 1:"+str(workingTemp1), font=("Helvetica",16))
+workingTemp1Display.pack(side = tk.LEFT)
+workingTemp2Display = tk.Label(root,text="Temp 2:"+str(workingTemp2), font=("Helvetica",16))
+workingTemp2Display.pack(side = tk.LEFT)
+workingTemp3Display = tk.Label(root,text="Temp 3:"+str(workingTemp3), font=("Helvetica",16))
+workingTemp3Display.pack(side = tk.LEFT)
+workingTemp4Display = tk.Label(root,text="Temp 4:"+str(workingTemp4), font=("Helvetica",16))
+workingTemp4Display.pack(side = tk.LEFT)
+
+
+#make the buffers
 serBuffer = ""
+tempBuffer = ""
+tempCounter = 1
 
 def readSerial():
     while True:
-        c = ser.read() # attempt to read a character from Serial
+        c = ser.read().decode("ascii") # attempt to read a character from Serial
         
         #was anything read?
         if len(c) == 0:
             break
         
-        # get the buffer from outside of this function
+        # get the buffers from outside of this function
         global serBuffer
+        global tempBuffer
+        global tempCounter
+        
+        # get the working temps from outside this function
+        global workingTemp1
+        global workingTemp2
+        global workingTemp3
+        global workingTemp4
         
         # check if character is a delimeter
         if c == '\r':
             c = '' # don't want returns. chuck it
             
         if c == '\n':
-            serBuffer += "\n" # add the newline to the buffer
-            
+            #serBuffer += "\n" # add the newline to the buffer
             #add the line to the TOP of the log
-            log.insert('0.0', serBuffer)
+            log.insert(tk.END, serBuffer)
             serBuffer = "" # empty the buffer
+            
+        if c == ',':
+            
+            if tempCounter == 1:
+                workingTemp1 = tempBuffer
+                tempCounter = 2
+                tempBuffer = ""
+                serBuffer += c
+                
+            elif tempCounter == 2:
+                workingTemp2 = tempBuffer
+                tempCounter = 3
+                tempBuffer = ""
+                serBuffer += c
+                
+            elif tempCounter == 3:
+                workingTemp3 = tempBuffer
+                tempCounter = 4
+                tempBuffer = ""
+                serBuffer += c
+                
+            elif tempCounter == 4:
+                workingTemp4 = tempBuffer
+                tempCounter = 1
+                tempBuffer = ""
+                serBuffer += c
+            
         else:
-            serBuffer += c # add to the buffer
+            serBuffer += c # add to the serial buffer
+            tempBuffer += c
     
     root.after(10, readSerial) # check serial again soon
 
+def update_display():
+    workingTemp1Display.config(text="Temp 1:"+str(workingTemp1))
+    workingTemp2Display.config(text="Temp 2:"+str(workingTemp2))
+    workingTemp3Display.config(text="Temp 3:"+str(workingTemp3))
+    workingTemp4Display.config(text="Temp 4:"+str(workingTemp4))
+    
+    root.after(1,update_display)
 
 # after initializing serial, an arduino may need a bit of time to reset
 root.after(100, readSerial)
+root.after(1,update_display)
 
 root.mainloop()
