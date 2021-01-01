abe0ac51703c4067212b47849f4064b73da6cc52
Kevin Rozmiarek <kevrozmiarek@gmail.com>
2020-12-31 17:59:25 -0700

    Added recording and new icon

--- recorder_gui.py
+++ recorder_gui.py
@@ -4,10 +4,16 @@ Created on Mon Dec 14 13:19:02 2020
 
 @author: Kevin Rozmiarek
 """
+# pyinstaller --icon=instaar_temp.ico --noconsole recorder_gui.py
 
 import serial as serial
 import tkinter as tk
-import datetime 
+import datetime
+import os
+import pathlib
+
+#Serial start and com management 
+###############################################################################
 
 try:
     serialPort = 'COM6' #Will change based on port plugged into
@@ -23,11 +29,77 @@ def disconnect():
 
 def restartSerial():
     global ser
-    log.insert(tk.END, 'Restarting comms')
+    log.insert(tk.END, ' \nRestarting comms')
     ser.close()
     serialPort = 'COM6' #Will change based on port plugged into
     baudRate = 9600
     ser = serial.Serial(serialPort, baudRate, timeout=0, writeTimeout=0) #ensure non-blocking
+
+#Recording
+###############################################################################
+
+recording = False
+
+def dataWindow():
+    
+   def dataWindowClose():
+       dataWindow.destroy()
+       
+   def buttonFunctions():
+       global recording
+       recording=True
+       recordSpeed = int(speedNumberEntry.get())
+       fileName = str(runNumberEntry.get())
+       root.after(1, lambda: recordData(recordSpeed, fileName))
+       log.insert(tk.END, '\nStarting recording')
+       dataWindowClose()
+       
+   dataWindow = tk.Toplevel()
+   dataWindow.geometry('570x220')
+   
+   dataWindowLabel1 = tk.Label(dataWindow, text= 'Please enter file name:',font=("Helvetica",16), height=2,width=34)
+   dataWindowLabel1.pack()
+   runNumberEntry = tk.Entry(dataWindow,font=("Helvetica",16))
+   runNumberEntry.pack()
+   
+   dataWindowLabel2 = tk.Label(dataWindow, text= 'How often do you want to record data? (1-??? seconds)',font=("Helvetica",16), height=2,width=44)
+   dataWindowLabel2.pack()
+   speedNumberEntry = tk.Entry(dataWindow,font=("Helvetica",16))
+   speedNumberEntry.pack()
+   
+   dataWindowCloseButton = tk.Button(dataWindow, text = 'Ok',height=1,font=("Helvetica",16),width = 25, command = buttonFunctions)
+   dataWindowCloseButton.pack()  
+
+def recordData(recordSpeed, fileName):
+    try:
+        #os.chdir("/home/sil/Documents/Fixed_wing_ground_station_code/instaar/Boundary_Layer_Prediction/data")
+        os.chdir(pathlib.Path(__file__).parent.absolute()/'data')
+    except OSError:
+        filler=1
+        #log.insert(tk.END, '\nCannot find data folder, make one called "data"!')
+    
+    recordFileName=str(fileName)+".txt"
+    recordFile  = open(recordFileName, "a")  
+    currentDT = datetime.datetime.now()
+    #decTime = (float(currentDT.minute)*60+float(currentDT.second))/3600
+    recordFile.write(\
+            str(currentDT)+','+\
+            str(workingTemp1)+','+\
+            str(workingTemp2)+','+\
+            str(workingTemp3)+','+\
+            str(workingTemp4)+'\n')  #Write string for data file!!!!!!
+    recordFile.close()
+    global recording
+    if recording:
+        root.after(recordSpeed*1000, lambda: recordData(recordSpeed, fileName))
+
+def stopRecording():
+    global recording
+    recording = False
+    log.insert(tk.END,'\nRecording ended')
+
+#The GUI
+###############################################################################
 
 #make a TkInter Window
 root = tk.Tk()
@@ -38,7 +110,7 @@ scrollbar = tk.Scrollbar(root)
 scrollbar.pack(side = tk.RIGHT, fill = tk.Y)
 
 # make a text box to put the serial output
-log = tk.Text(root, width=60, height=30, takefocus=0)
+log = tk.Text(root, width=40, height=20, takefocus=0)
 log.pack()
 
 # attach text box to scrollbar
@@ -46,14 +118,22 @@ log.config(yscrollcommand=scrollbar.set)
 scrollbar.config(command=log.yview)
 
 #Button to restart serial
-restartButton = tk.Button(root, text="Restart Serial Comms", command = restartSerial)
+restartButton = tk.Button(root, text="Restart Serial Comms", font=("Helvetica",14), command = restartSerial)
 restartButton.pack()
 
+#Button to start recording
+startRecordingButton = tk.Button(root, text="Start recording", font=("Helvetica",14),  command = dataWindow)
+startRecordingButton.pack()
+
+#Button to stop recording
+stopRecordingButton = tk.Button(root, text="Stop recording", font=("Helvetica",14),  command = stopRecording)
+stopRecordingButton.pack()
+
 #Button to close correctly
-closeButton = tk.Button(root, text="Exit", command = disconnect)
+closeButton = tk.Button(root, text="Exit", font=("Helvetica",14), command = disconnect)
 closeButton.pack()
 
-#Temperature Displays
+#Temperature display
 workingTemp1 = ""
 workingTemp2 = ""
 workingTemp3 = ""
@@ -68,6 +148,19 @@ workingTemp3Display.pack(side = tk.LEFT)
 workingTemp4Display = tk.Label(root,text="Temp 4:"+str(workingTemp4), font=("Helvetica",16))
 workingTemp4Display.pack(side = tk.LEFT)
 
+#GUI Updating
+###############################################################################
+
+def update_display():
+    workingTemp1Display.config(text="Temp 1:\n"+str(workingTemp1))
+    workingTemp2Display.config(text="Temp 2:\n"+str(workingTemp2))
+    workingTemp3Display.config(text="Temp 3:\n"+str(workingTemp3))
+    workingTemp4Display.config(text="Temp 4:\n"+str(workingTemp4))
+    
+    root.after(1,update_display)
+
+#Serial data handling
+###############################################################################
 
 #make the buffers
 serBuffer = ""
@@ -100,13 +193,14 @@ def readSerial():
         if c == '\n':
             #serBuffer += "\n" # add the newline to the buffer
             #add the line to the TOP of the log
-            log.insert(tk.END, serBuffer)
+            #log.insert(tk.END, serBuffer) If you want to insert the readlout to the log
             serBuffer = "" # empty the buffer
+            tempBuffer = ""
             
         if c == ',':
             
             if tempCounter == 1:
-                workingTemp1 = tempBuffer
+                workingTemp1 = tempBuffer[1:]
                 tempCounter = 2
                 tempBuffer = ""
                 serBuffer += c
@@ -134,17 +228,11 @@ def readSerial():
             tempBuffer += c
     
     root.after(10, readSerial) # check serial again soon
-
-def update_display():
-    workingTemp1Display.config(text="Temp 1:"+str(workingTemp1))
-    workingTemp2Display.config(text="Temp 2:"+str(workingTemp2))
-    workingTemp3Display.config(text="Temp 3:"+str(workingTemp3))
-    workingTemp4Display.config(text="Temp 4:"+str(workingTemp4))
     
+###############################################################################
+
+if __name__ == '__main__':
+    root.after(100, readSerial)
     root.after(1,update_display)
 
-# after initializing serial, an arduino may need a bit of time to reset
-root.after(100, readSerial)
-root.after(1,update_display)
-
-root.mainloop()
+    root.mainloop()
